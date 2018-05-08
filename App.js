6621f7cf5be9f7738566b360831fa14b36334739
Bridgen Parekh <bridgen.parekh24@gmail.com>
2018-05-08 14:16:38 -0400

    Add files via upload
    Successfully can add a todo

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import AddTodo from './components/addTodo';
+import AddTodo from './components/addTodo/';
+import TodoList from './components/todoList';
 import actions from './actions/';
 
-export const App = ({submitTodo}) => (
+export const App = ({ submitTodo, todos }) => (
   <div>
     <h1>Todo list</h1>
-    <AddTodo submitTodo = {submitTodo} />
+    <AddTodo submitTodo={submitTodo} />
+    <TodoList todos={todos} />
   </div>
 );
 
-App.PropTypes = {
-	submitTodo: PropTypes.func.isRequired,
+App.propTypes = {
+  submitTodo: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(PropTypes.shape(
+    {
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    },
+  )).isRequired,
 };
 
 const mapStateToProps = state => state.todoListApp;
 
 const mapDispatchToProps = dispatch => ({
-	submitTodo: (text) => {
-		if (text) {
-			dispatch(actions.submitTodo(text));
-		}
-	},
+  submitTodo: (text) => {
+    if (text) {
+      dispatch(actions.submitTodo(text));
+    }
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
